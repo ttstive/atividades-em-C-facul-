adaba45c318474f5bbcd651518ff9852a76c86f0
Estevão Lins Maia <114420545+ttstive@users.noreply.github.com>
2023-09-28 09:54:34 -0300

    Update atividade_5.c

--- atividade_avaliativa/atividade_5.c
+++ atividade_avaliativa/atividade_5.c
@@ -14,7 +14,7 @@ int main(){
 
     else if (numero1 > numero2){
         printf("%d eh maior que %d\n", numero1, numero2);
-        if((numero1 % numero2) == 0){
+        if((numero2 != 0) && (numero1 % numero2) == 0){
             printf("%d eh multiplo de %d\n", numero1, numero2);
         }
 
@@ -25,7 +25,7 @@ int main(){
 
     else if(numero1 < numero2){
         printf("%d eh maior que %d\n", numero2, numero1);
-        if((numero2 % numero1) == 0){
+        if((numero1 != 0) && (numero2 % numero1) == 0){
             printf("%d eh multiplo de %d\n", numero2, numero1);
         }
 
