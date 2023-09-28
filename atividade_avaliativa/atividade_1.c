73719d03d78ac37a9d4207dabba0867eabcaa02c
Estevão Lins Maia <114420545+ttstive@users.noreply.github.com>
2023-09-28 09:20:18 -0300

    Update atividade_1.c

--- atividade_avaliativa/atividade_1.c
+++ atividade_avaliativa/atividade_1.c
@@ -5,15 +5,15 @@ int main (){
     int ano;
     scanf("%d", &ano);
 
-    if ((ano >= 1986 && ano <=2022) && (ano %4 == 0) && (ano != 1916) && (ano != 1944)) {
-        printf("Os jogos olimpicos de verao ocorreram no ano de %d.\n", ano);
+    if ((ano >= 1886 && ano <=2022) && (ano %4 == 0) && (ano != 1916) && (ano!= 1940) && (ano != 1944)) {
+        printf("Os jogos Olimpicos de verao ocorreram no ano de %d.\n", ano);
     }
     
     else if (ano >= 1930 && (ano - 1930) % 4 == 0 && (ano != 1942) && (ano != 1946)) {
-        printf("A Copa do mundo de futebol ocorreu no ano de %d.\n", ano);
+        printf("A Copa do Mundo de futebol ocorreu no ano de %d.\n", ano);
     } 
     else{
-        printf("“Não houve Jogos Olímpicos de Verão ou Copa do Mundo no ano de %d");
+        printf(“Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d");
     }
    
 
