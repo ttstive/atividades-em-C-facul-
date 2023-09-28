9d8fb2e6cd8c3b0cdeb91384db2ce260531f7c80
Estevão Lins Maia <114420545+ttstive@users.noreply.github.com>
2023-09-28 09:46:58 -0300

    Update atividade_3.c

--- atividade_avaliativa/atividade_3.c
+++ atividade_avaliativa/atividade_3.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 int main (){
-    int senha_correta, senha__que_foi_inserida;
+    int senha_correta, senha_que_foi_inserida;
     scanf("%d", &senha_correta);
-      printf("senha cadastrada:", senha_correta);
+    printf("senha cadastrada: %d\n", senha_correta);
 
     while (senha_que_foi_inserida != senha_correta)
 {
-    scanf("%d", &senha_que_foi_inserida;
-     if (senha_inserida == senha_correta) {
+    scanf("%d", &senha_que_foi_inserida);
+     if (senha_que_foi_inserida == senha_correta) {
             printf("senha valida!\n");
 
         } else {
